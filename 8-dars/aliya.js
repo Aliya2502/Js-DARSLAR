ceed40d4410ca1edea49e88532587a122deae4d9
Aliya2502 <faritarslan10@gmail.com>
2025-04-21 16:02:30 +0500

    Add files via upload

---
+++ 8-dars/aliya.js
@@ -0,0 +1,28 @@
+"1-png"
+
+// min (2, 5 ) == 2
+// min (3, -1 ) == -1
+// min (1, 1 ) == 1
+ 
+console.log(Math.min(2, 5));   // Natija: 2
+console.log(Math.min(3, -1));  // Natija: -1
+console.log(Math.min(1, 1));   // Natija: 1
+
+
+
+
+"2-png"
+
+// pow (3, 2 ) =3*3 = 9
+// pow (3, 3 ) =3*3*3= 27
+// pow (1, 100 ) = 1*1* ...*1=1 
+
+
+console.log(Math.pow(3, 2));    //natija // 9
+console.log(Math.pow(3, 3));    //natija // 27
+console.log(Math.pow(1, 100));  //natija // 1
+
+
+
+
+
