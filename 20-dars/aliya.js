21d1b7d4c2fc2f7a4440291e12f4975d9bdfeddf
Aliya2502 <faritarslan10@gmail.com>
2025-05-25 14:56:47 +0500

    Add files via upload

---
+++ 20-dars/aliya.js
@@ -0,0 +1,41 @@
+// let people = [
+//     { id: 1, firstName: "Eshmat", lastName: "Toshmatov", age: 20, status: "Talaba" },
+//     { id: 2, firstName: "Gulmat", lastName: "Surmatov", age: 17, status: "O'quvchi" },
+//     { id: 3, firstName: "Dilshod", lastName: "Karimov", age: 25, status: "Ishchi" },
+//     { id: 4, firstName: "Malika", lastName: "Xoliqova", age: 30, status: "Uqituvchi" },
+//     { id: 5, firstName: "Zafar", lastName: "Bozorov", age: 19, status: "Talaba" },
+//     { id: 6, firstName: "Umida", lastName: "Raxmatova", age: 22, status: "Tadbirkor" },
+//     { id: 7, firstName: "Rustam", lastName: "Ziyodov", age: 35, status: "Dasturchi" },
+//     { id: 8, firstName: "Nodira", lastName: "Saidova", age: 28, status: "Muhandis" },
+//     { id: 9, firstName: "Javlon", lastName: "Qurbonov", age: 24, status: "Frilanser" },
+//     { id: 10, firstName: "Shahnoza", lastName: "Ergasheva", age: 18, status: "O'quvchi" }
+//   ];
+  
+//   // 1
+//   people = people.map(p =>
+//     p.id === 5
+//       ? { id: 5, firstName: "Ali", lastName: "Valiyev", age: 25, status: "Dasturchi" }
+//       : p
+//   );
+//   console.log(people);
+  
+// //   // 2
+// //   people = people.filter(p => p.lastName.endsWith("va"));
+// //   console.log(people);
+  
+// //   // 3
+// //   people = people.filter(p => p.id !== 4 && p.id !== 9);
+
+// //   console.log(people);
+  
+// //   // 4
+// //   people = people.map(p =>
+// //     p.id === 7 ? { ...p, firstName: "Gulbashakar" } : p
+// //   );
+// //   console.log(people);
+  
+// //   // 5
+// //   people.sort((a, b) => a.lastName.localeCompare(b.lastName));
+
+// //   console.log(people);
+  
