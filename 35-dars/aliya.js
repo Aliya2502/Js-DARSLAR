198735d35b983885fe3f922f8d2266b4572c0d1e
Aliya2502 <faritarslan10@gmail.com>
2025-07-11 15:35:32 +0500

    Add files via upload

---
+++ 35-dars/aliya.js
@@ -0,0 +1,71 @@
+
+// homework
+// let webbrain = {
+//     frontend: [
+//       { course: "react", price: 120 },
+//       { course: "vue", price: 110 },
+//       { course: "angular", price: 100 },
+//     ],
+//     backend: [
+//       { course: "java", price: 120 },
+//       { course: "php", price: 110 },
+//       { course: "nodejs", price: 100 },
+//     ],
+//     mobile: {
+//       android: [
+//         { course: "android", price: 120 },
+//         { course: "flutter", price: 110 },
+//       ],
+//       ios: [
+//         { course: "ios", price: 120 },
+//         { course: "flutter", price: 110 },
+//       ],
+//     },
+//   };
+  
+//   function getTotalPrice(obj) {
+//     let sum = 0;
+  
+//     if (Array.isArray(obj)) {
+//       for (let item of obj) {
+//         sum += item.price;
+//       }
+//     } else if (typeof obj === "object") {
+//       for (let key in obj) {
+//         sum += getTotalPrice(obj[key]);
+//       }
+//     }
+  
+//     return sum;
+//   }
+  
+//   console.log(getTotalPrice(webbrain)); 
+  
+
+
+
+
+
+// function* myGenerator() {
+//     yield "Hello";
+//     yield "World";
+//     return "Done";
+//   }
+  
+
+
+// let getsumm=(a)=> {
+//     return(b)=>{
+//         return(c)=>{
+//             return(d)=>{
+//                 console.log(a+b+c+d);
+                
+//             }
+//         }
+//     }
+// }
+
+
+// getsumm(1)(2)(3)(4);
+
+
